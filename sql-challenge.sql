2a50a8e0bc8deaf5c688e8dd69f3203b174ae127
Ana Maria Di Persio <59493744+adipersio@users.noreply.github.com>
2020-03-28 17:46:14 -0500

    Add files via upload

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -1,54 +1,3 @@
-CREATE TABLE "departments" (
-    dept_no VARCHAR PRIMARY KEY,
-    dept_name VARCHAR NOT NULL
-);
-
-
-CREATE TABLE "dept_emp" (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-
-CREATE TABLE "dept_manager" (
-    dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-CREATE TABLE "employees" (
-    emp_no SERIAL PRIMARY KEY,
-    birth_date VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL
-);
-
-CREATE TABLE "salaries" (
-    emp_no INT NOT NULL,
-    salary INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-CREATE TABLE "titles" (
-    emp_no INT NOT NULL,
-    titles VARCHAR NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-------------------------------------------------------
 1.
 SELECT employees.emp_no, employees.first_name, employees.last_name, employees.gender, salaries.salary 
 FROM employees
@@ -67,9 +16,6 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31' ;
 -- List the manager of each department with department number, department name, 
 -- the manager's employee number, last name, first name, and start and end employment dates
 3. 
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM departments;
 
 SELECT employees.emp_no, 
 employees.first_name, 
@@ -87,4 +33,72 @@ ORDER BY dept_manager.dept_no;
 
 ------------------------------------------------------
 4.
+-- List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name
+
+SELECT employees.emp_no, 
+employees.first_name, 
+employees.last_name, 
+departments.dept_name
+
+FROM employees
+INNER JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
+INNER JOIN departments ON dept_manager.dept_no=departments.dept_no;
+
+------------------------------------------------------
+5.
+-- List all employees whose first name is "Hercules" and last names begin with "B"
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules' AND 
+last_name LIKE 'B%';
+
+------------------------------------------------------
+6.
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT employees.first_name, 
+employees.last_name, 
+departments.dept_name
+
+FROM employees
+INNER JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
+INNER JOIN departments ON dept_manager.dept_no=departments.dept_no
+
+WHERE dept_name = 'Sales';
+
+
+------------------------------------------------------
+7.
+-- List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+
+SELECT employees.emp_no,
+employees.first_name, 
+employees.last_name, 
+departments.dept_name
+
+FROM employees
+INNER JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
+INNER JOIN departments ON dept_manager.dept_no=departments.dept_no
+
+WHERE dept_name = 'Sales' OR 
+dept_name = 'Development';
+
+
+------------------------------------------------------
+8.
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) AS last_name_count
+FROM employees
+GROUP BY last_name
+ORDER BY last_name DESC;
+
+
+-----------------------------------------------------------
+-- Just jokes
+SELECT *
+FROM employees
+WHERE emp_no = 499942;
 
